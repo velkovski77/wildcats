f8f684980905266945670c1d34e6d2438ec2f181
dragan777 <draganvelkovski77@gmail.com>
2013-07-17 13:23:51 +0200

    Change a quite

--- BeatyOfNature/Controllers/HomeController.cs
+++ BeatyOfNature/Controllers/HomeController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BeatyOfNature.Models;
 namespace BeatyOfNature.Controllers
 {
     public class HomeController : Controller
     {
+        private WildCatsDataContext db = new WildCatsDataContext();
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to Beauty of Nature";
 
-            return View();
+            return View(db.WildCats.ToList());
         }
 
         public ActionResult About()
