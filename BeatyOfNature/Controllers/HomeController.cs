679f25b6044879dfe9e19940183933d56fa61982
dragan777 <draganvelkovski77@gmail.com>
2013-07-17 11:27:17 +0200

    Adding Page

---
+++ BeatyOfNature/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeatyOfNature.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Beauty of Nature";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
---
+++ BeatyOfNature/Scripts/myScript.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#fade').list_ticker({
+        speed: 9000,
+        effect: 'fade'
+    });
+    $('#slide').list_ticker({
+        speed: 9000,
+        effect: 'slide'
+    });
+})
