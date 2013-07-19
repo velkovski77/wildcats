000506713280949a5fdb0165e72d66b1ef777f8d
dragan777 <draganvelkovski77@gmail.com>
2013-07-19 11:00:06 +0200

    A few edits

--- BeatyOfNature/Scripts/myScript.js
+++ BeatyOfNature/Scripts/myScript.js
@@ -7,4 +7,18 @@
         speed: 9000,
         effect: 'slide'
     });
-})
+
+    //limit  text to show
+  
+    	$(function(){
+    	    var $elem = $('.textinspan'); 		// The element or elements with the text to hide
+    	    var $limit = 300;		// The number of characters to show
+    	    var $str = $elem.html();	// Getting the text
+    	    var $strtemp = $str.substr(0,$limit);	// Get the visible part of the string
+    	    $str = $strtemp + '<span class="hide">' + $str.substr($limit,$str.length) + '</span>';	// Recompose the string with the span tag wrapped around the hidden part of it
+    	    $elem.html($str);		// Write the string to the DOM 
+    	})
+    
+
+});
+
